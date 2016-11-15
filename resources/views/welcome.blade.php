02b6516b17db15cc7e03d0d09980b20cab771e03
Fikri Abdul <fikri.abdul@gmx.com>
2016-11-15 12:09:03 +0700

    modified welcome page in the resource folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Laravel and S3 Amazon AWS</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 
         <style>
             html, body {
@@ -38,7 +39,20 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5 and S3 Amazon AWS</div>
+                <div class="row">
+                  {!! Form::open(['url' => 'welcome/upload', 'method'=>'POST', 'files'=>'true']) !!}
+                    <div class="form-group col-md-12">
+                      {!! Form::label('Upload an image') !!}
+                      {!! Form::file('file') !!}
+                    </div><br/><hr/>
+                    <div class="form-group col-md-12">
+                      {!! Form::submit('Create Information', ['class' => 'btn btn-success form-control']) !!}
+                    </div>
+
+                  {!! Form::close() !!}
+                </div>
+
             </div>
         </div>
     </body>
